bcb10649f95c19128914165dacb90cca455881ba
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2014-04-05 23:47:29 +1100

    Return full report details for current incidents endpoint.

--- api.go
+++ api.go
@@ -9,7 +9,11 @@ func latestReportsForCurrentIncidents() (reports []ReportFeature, err error) {
 	// NOTE some of this is copied from reportFeatureForUUID
 
 	// Select the latest report for all current incidents
-	stmt, err := db.Prepare(`SELECT DISTINCT ON (i.uuid) r.uuid, r.title, ST_Y(r.geometry) as lat, ST_X(r.geometry) as lng
+	stmt, err := db.Prepare(`SELECT DISTINCT ON (i.uuid) r.uuid, incident_uuid,
+                              guid, title, link, category, pubdate,
+                              updated, alert_level, location, council_area, status, fire_type,
+                              fire, size, responsible_agency, extra,
+                              ST_Y(r.geometry) as lat, ST_X(r.geometry) as lng
                             FROM incidents i
                             JOIN reports r ON i.uuid = r.incident_uuid
                             WHERE i.current = true
@@ -30,7 +34,7 @@ func latestReportsForCurrentIncidents() (reports []ReportFeature, err error) {
 		var rp ReportProperties
 		var rf ReportFeature
 
-		err = rows.Scan(&rf.UUID, &rp.Title, &lat, &lng)
+		err = rows.Scan(&rf.UUID, &rp.IncidentUUID, &rp.Guid, &rp.Title, &rp.Link, &rp.Category, &rp.Pubdate, &rp.Updated, &rp.AlertLevel, &rp.Location, &rp.CouncilArea, &rp.Status, &rp.FireType, &rp.Fire, &rp.Size, &rp.ResponsibleAgency, &rp.Extra, &lat, &lng)
 		if err != nil {
 			return
 		}
